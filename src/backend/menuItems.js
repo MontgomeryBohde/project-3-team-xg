6ea619b10a0da6314d89af543141b2f986f83847
Montgomery Bohde <83386908+MontgomeryBohde@users.noreply.github.com>
2024-11-04 11:14:24 -0600

    ADD: Basic Database Connection/Query

---
+++ src/backend/menuItems.js
@@ -0,0 +1,33 @@
+"use server";
+
+import { Client } from 'pg';
+
+export default async function getMenuItems() {
+    const connectionString = process.env.POSTGRES_URL;
+    console.log('Creating Connection to:', connectionString);
+
+    const client = new Client({
+        connectionString: connectionString,
+        connectionTimeoutMillis: 10000, // Increase timeout to 10 seconds
+    });
+
+    try {
+        console.log('Awaiting Connection');
+        await client.connect();
+        console.log('Connection Established');
+
+        console.log('Fetching Menu Items');
+        const result = await client.query('SELECT * FROM menu_items;');
+        console.log('Menu items fetched:');
+        console.log(result.rows);
+
+        return result.rows;
+    } catch (error) {
+        console.error('Error during database operation:', error);
+        throw new Error('Failed to fetch menu items');
+    } finally {
+        console.log('Closing Connection');
+        await client.end();
+        console.log('Connection Closed');
+    }
+}
