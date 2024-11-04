f5b8c2f389ebdee5354c23bb2997c63c5035c2e5
Rebecca <rebecca7@tamu.edu>
2024-11-04 01:04:02 -0600

    CREATE: reports navigation page, other report dirs

--- src/app/employee/manager/reports/page.js
+++ src/app/employee/manager/reports/page.js
@@ -1,1 +1,49 @@
 // reports menu page
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import EmployeeHeader from "@/components/ui/employee/header/EmployeeHeader";
+import Head from "next/head";
+
+const Page = () => {
+    const router = useRouter();
+
+    const navigateToSales = () => {
+        router.push("/employee/manager/reports/sales"); // Navigate to the reports menu page
+    };
+    const navigateToDailyReports = () => {
+        router.push("/employee/manager/reports/sales"); // Navigate to the reports menu page
+    };
+    const navigateToMenuPopularity = () => {
+        router.push("/employee/manager/reports/sales"); // Navigate to the reports menu page
+    };
+    const navigateToProductUsage = () => {
+        router.push("/employee/manager/reports/sales"); // Navigate to the reports menu page
+    };
+
+    return (
+        <>
+            <Head>
+                <title>Reports</title>
+            </Head>
+            <EmployeeHeader />
+            <div className="reportsContainer text-center">
+                {/* Navigation Button */}
+                <button className="btn btn-primary btn-lg m-3" onClick={navigateToSales}>
+                    Sales Report
+                </button>
+                <button className="btn btn-primary btn-lg m-3" onClick={navigateToDailyReports}>
+                    Daily Reports
+                </button>
+                <button className="btn btn-primary btn-lg m-3" onClick={navigateToMenuPopularity}>
+                    Menu Items Popularity
+                </button>
+                <button className="btn btn-primary btn-lg m-3" onClick={navigateToProductUsage}>
+                    Product Usage Chart
+                </button>
+            </div>
+        </>
+    );
+};
+
+export default Page;
