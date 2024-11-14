d29b4efb9829019fb49e11b251e35c1afd6c4bf1
Montgomery Bohde <83386908+MontgomeryBohde@users.noreply.github.com>
2024-11-14 10:01:32 -0600

    FIX: Import error

--- src/app/customer/kiosk/cart/page.js
+++ src/app/customer/kiosk/cart/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import dynamic from 'next/dynamic'
-const CartPage = dynamic(() => import('./CartPage'), { ssr: false });
+const CartPage = dynamic(() => import('./cart'), { ssr: false });
 
 const CartPageWrapper = () => {
     return (
