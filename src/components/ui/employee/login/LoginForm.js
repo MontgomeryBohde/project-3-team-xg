973fd5c8173d70d4ffc9d876cd022d64fa50f95b
Montgomery Bohde <83386908+MontgomeryBohde@users.noreply.github.com>
2024-12-11 17:31:14 -0600

    FIX: Set logged in user on Github login

--- src/components/ui/employee/login/LoginForm.js
+++ src/components/ui/employee/login/LoginForm.js
@@ -61,7 +61,9 @@ const LoginForm = () => {
   };
 
   const handleGithubLogin = () => {
-    signIn("github", { callbackUrl: "/employee/home" });
+    const employee = employees.find(emp => emp.id.toString() === 2);
+    localStorage.setItem('loggedInEmployee', JSON.stringify(employee));
+    signIn("github", { callbackUrl: "/employee/home"});
   };
 
   return (
