2ed837a7a8537dc3dbbda449c912beed4a6153bb
RainSuds <1319837595@qq.com>
2024-12-11 21:21:26 -0600

    ADDED: comments for getMenu.js

--- pages/api/getInventory.js
+++ pages/api/getInventory.js
@@ -183,6 +183,9 @@ export default async function handler(req, res) {
                     }
                 }
 
+                /**
+                 * Default case for invalid action type.
+                 */
                 default:
                     return res.status(400).json({ error: 'Invalid action' });
             }
